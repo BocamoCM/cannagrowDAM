d0b440ca0e019a73acc6fa45ebef02e34a17b44c
ElTrompa <anroiz@alumnatflorida.es>
2025-05-06 13:57:19 +0200

    base de datos funcionable

--- BD archivos/ScriptVistesBD.sql
+++ BD archivos/ScriptVistesBD.sql
@@ -3,7 +3,7 @@ SELECT e.nombre, COUNT(p.id) AS total_pedidos FROM Empleado e
 JOIN Pedido p ON e.id = p.empleado_id
 GROUP BY e.nombre;
 
--- create view PedidoEmpleadoClienteProducto as
+create view PedidoEmpleadoClienteProducto as
 select e.nombre, c.nombre, pr.nombre, pe.id from Pedido pe
 join Empleado e on pe.empleado_id = e.id
 join Cliente c on pe.cliente_id = c.id
