78f90d3d1155acba661130671f93ca578af14b34
ElTrompa <anroiz@alumnatflorida.es>
2025-05-06 13:01:18 +0200

    1vista

--- BD archivos/ScriptVistesBD.sql
+++ BD archivos/ScriptVistesBD.sql
@@ -1,2 +1,4 @@
-
-select * from Empleado
+create view CantidadPedidosEmplado as
+SELECT e.nombre, COUNT(p.id) AS total_pedidos FROM Empleado e
+JOIN Pedido p ON e.id = p.empleado_id
+GROUP BY e.nombre;
