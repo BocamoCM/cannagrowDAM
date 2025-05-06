7a6fc653aff2eec22a44efb91f0781545fcdf5e7
ElTrompa <anroiz@alumnatflorida.es>
2025-05-06 13:51:16 +0200

    error 1452 ItemPedido

--- BD archivos/ScriptVistesBD.sql
+++ BD archivos/ScriptVistesBD.sql
@@ -2,3 +2,10 @@ create view CantidadPedidosEmplado as
 SELECT e.nombre, COUNT(p.id) AS total_pedidos FROM Empleado e
 JOIN Pedido p ON e.id = p.empleado_id
 GROUP BY e.nombre;
+
+-- create view PedidoEmpleadoClienteProducto as
+select e.nombre, c.nombre, pr.nombre, pe.id from Pedido pe
+join Empleado e on pe.empleado_id = e.id
+join Cliente c on pe.cliente_id = c.id
+join Producto pr on pe.empleado_id = pr.id
+join ItemPedido ip on pe.id = pedido_id;
