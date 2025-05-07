86a548de93e724b46857b16c444cdca5dabfec11
ElTrompa <anroiz@alumnatflorida.es>
2025-05-07 11:26:31 +0200

    paso anterior de pedidos

--- BD archivos/ScriptBaseDeDades.sql
+++ BD archivos/ScriptBaseDeDades.sql
@@ -29,7 +29,7 @@ CREATE TABLE Cliente (
 CREATE TABLE Empleado (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
-    rol ENUM('Vendedor', 'Cultivador', 'Gerente') NOT NULL,
+    rol ENUM('Vendedor', 'Cultivador', 'Gerente', 'Repartidor') NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     salario FLOAT NOT NULL,
     contrasena_hash CHAR(60) NOT NULL,
@@ -44,8 +44,10 @@ CREATE TABLE Pedido (
     estado ENUM('Pendiente', 'Enviado', 'Entregado', 'Cancelado') NOT NULL,
     cliente_id INT,
     empleado_id INT,
+    vehiculo_matricula VARCHAR(15) NOT NULL,
     FOREIGN KEY (cliente_id) REFERENCES Cliente(id),
-    FOREIGN KEY (empleado_id) REFERENCES Empleado(id)
+    FOREIGN KEY (empleado_id) REFERENCES Empleado(id),
+    FOREIGN KEY (vehiculo_matricula) REFERENCES Vehiculo(matricula)
 );
 
 -- Tabla ItemPedido
@@ -62,4 +64,16 @@ CREATE TABLE ItemPedido (
 CREATE TABLE Regulacion (
     id INT PRIMARY KEY AUTO_INCREMENT,
     descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Vehiculo (
+    matricula VARCHAR(15) PRIMARY KEY,
+    color VARCHAR(50) NOT NULL,
+    conductor_id INT NOT NULL,
+    marca VARCHAR(100) NOT NULL,
+    estado ENUM('Activo', 'En mantenimiento', 'Fuera de servicio') NOT NULL,
+    kilometros_totales INT NOT NULL,
+    consumo_100km FLOAT NOT NULL,
+    cv_motor INT NOT NULL,
+    FOREIGN KEY (conductor_id) REFERENCES Empleado(id)
 );
